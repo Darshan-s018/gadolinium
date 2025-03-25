5497790aa7a73f4d4a4f7c1d6c3d19c82aeb1e29
Darshan S <darshu8988@gmail.com>
2025-03-25 10:29:15 +0530

    refactor authentication module: consolidate types and implement sign-up and log-in functionality

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
-import jwt from "jsonwebtoken";
 import "dotenv/config";
+import { hono } from "./routes/routes";
+import { serve } from "@hono/node-server";
 
-// Removed incorrect import as dotenv will handle environment variables
-
-import {config} from "dotenv";
-
-const payload: jwt.JwtPayload = {
-  iss: "https://purpleshorts.co.in",
-  sub: "Darshans-018",
-};
-const jwtSecretKey = process.env.SECRET_KEY;
-console.log(jwtSecretKey);
-if (!jwtSecretKey) {
-  throw new Error("undefined");
-}
-const token = jwt.sign(payload, jwtSecretKey, {
-  algorithm: "HS256",
+serve(hono, (info) => {
+  console.log(`Server is running @ http://localhost:${info.port}`);
 });
-console.log(token);
-try {
-  const decodedPayload = jwt.verify(token, jwtSecretKey);
-  console.log(decodedPayload);
-}
-catch (e) {
-  console.log("error",e);
-}
